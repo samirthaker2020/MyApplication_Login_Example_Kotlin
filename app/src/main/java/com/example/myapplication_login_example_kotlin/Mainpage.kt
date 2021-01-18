9913d4dc027889fb90ac457be5948d9d976f3510
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-18 18:44:38 -0500

    added main landing page

---
+++ app/src/main/java/com/example/myapplication_login_example_kotlin/Mainpage.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication_login_example_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mainpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mainpage)
+    }
+}
