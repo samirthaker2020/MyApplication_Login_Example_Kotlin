560774992416387522ba223c1af05976cc2c2181
samirthaker2020 <46947688+samirthaker2020@users.noreply.github.com>
2021-01-18 18:47:37 -0500

    fetch control id's

--- app/src/main/java/com/example/myapplication_login_example_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/myapplication_login_example_kotlin/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.myapplication_login_example_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // fetch conteol ids
+        val btnlogin=findViewById<Button>(R.id.btn_login);
+        val email= findViewById<EditText>(R.id.editText_emailid);
+        val password=findViewById<EditText>(R.id.editText_password);
+        
+
     }
 }
